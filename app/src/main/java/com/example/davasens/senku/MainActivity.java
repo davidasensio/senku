a2babe5513e49364a54d9da0cf7ab1d9bd610d98
davidasensio <davidasensio81@gmail.com>
2015-03-11 11:52:53 +0100

    added todo

--- app/src/main/java/com/example/davasens/senku/MainActivity.java
+++ app/src/main/java/com/example/davasens/senku/MainActivity.java
@@ -27,6 +27,7 @@ import java.util.TreeSet;
  *      - Icono con efecto 3d
  *      - Demo
  *      - Mas layouts: cruz, circulo, rombo...
+ *      - Pulbish in Git
  *
  */
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
